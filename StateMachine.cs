f0aed31f32588c31df77d4af60ab5aa160480362
Jimmy Cushnie <jimmycushnie@gmail.com>
2019-03-03 23:23:26 -0500

    made some changes

--- StateMachine.cs
+++ StateMachine.cs
@@ -4,74 +4,39 @@ using System.Collections.Generic;
 namespace PiMachine
 {
     public class StateMachine<TState, TTrigger>
+        where TState : Enum
+        where TTrigger : Enum
     {
         public TState CurrentState { get; private set; }
 
         private IList<Permit> Permits = new List<Permit>();
-        private IDictionary<TState, Action> OnExit = new Dictionary<TState, Action>();
-        private IDictionary<TState, Action> OnEnter = new Dictionary<TState, Action>();
+        private IDictionary<TState, Action> OnRun = new Dictionary<TState, Action>();
 
-        public delegate void StateDelegate(TState state);
-        public event StateDelegate EnteredState;
-        public event StateDelegate ExitedState;
-
-        public StateMachine() : this(default(TState))
-        {
-        }
-
-        public StateMachine(TState initialState)
+        public StateMachine(TState initialState = default)
         {
             this.CurrentState = initialState;
-
-            if (OnEnter.TryGetValue(initialState, out var onEnter))
-                onEnter();
         }
 
         public Configurer Configure(TState state) => new Configurer(state, this);
-
-        public void PermitFromAny(TTrigger trigger, TState state)
-            => PermitFromAny(trigger, state, () => true);
-
-        public void PermitFromAny(TTrigger trigger, TState state, Action execute)
-            => PermitFromAny(trigger, state, () => { execute(); return true; });
-
-        public void PermitFromAny(TTrigger trigger, TState state, Func<bool> condition)
-        {
-            Permits.Add(new Permit(trigger, state, condition));
-        }
 
         public void Fire(TTrigger trigger)
         {
             foreach (var permit in Permits)
             {
-                if ((permit.FromAny || permit.Original.Equals(CurrentState)) && permit.Trigger.Equals(trigger))
+                if (permit.Original.Equals(CurrentState) && permit.Trigger.Equals(trigger))
                 {
-                    //Check if we are allowed to change state
-                    bool? cont = permit.Execute?.Invoke();
-
-                    //If not, break out
-                    if (cont == false)
-                        break;
-
-                    //Fire the configuration's exit method
-                    if (OnExit.TryGetValue(CurrentState, out var onExit))
-                        onExit();
-
-                    //Fire the exit event
-                    ExitedState?.Invoke(CurrentState);
-
+                    permit.Execute?.Invoke();
                     this.CurrentState = permit.NextState;
-
-                    //Fire the configuration's enter method
-                    if (OnEnter.TryGetValue(CurrentState, out var onEnter))
-                        onEnter();
-
-                    //Fire the enter event
-                    EnteredState?.Invoke(CurrentState);
 
                     break;
                 }
             }
+        }
+
+        public void RunCurrentState(TState state)
+        {
+            OnRun.TryGetValue(state, out var action);
+            action?.Invoke();
         }
 
         private struct Permit
@@ -79,25 +44,14 @@ namespace PiMachine
             public TState Original;
             public TTrigger Trigger;
             public TState NextState;
-            public Func<bool> Execute;
-            public bool FromAny;
+            public Action Execute;
 
-            public Permit(TState original, TTrigger trigger, TState nextState, Func<bool> execute)
+            public Permit(TState original, TTrigger trigger, TState nextState, Action execute)
             {
                 this.Original = original;
                 this.Trigger = trigger;
                 this.NextState = nextState;
                 this.Execute = execute;
-                this.FromAny = false;
-            }
-
-            public Permit(TTrigger trigger, TState nextState, Func<bool> execute)
-            {
-                this.Original = default(TState);
-                this.Trigger = trigger;
-                this.NextState = nextState;
-                this.Execute = execute;
-                this.FromAny = true;
             }
         }
 
@@ -112,31 +66,15 @@ namespace PiMachine
                 this.Machine = machine;
             }
 
-            public Configurer OnExit(Action action)
+            public Configurer OnRun(Action action)
             {
-                Machine.OnExit[State] = action;
-
+                Machine.OnRun[State] = action;
                 return this;
             }
 
-            public Configurer OnEnter(Action action)
+            public Configurer Permit(TTrigger trigger, TState changeTo, Action execute = null)
             {
-                Machine.OnEnter[State] = action;
-
-                return this;
-            }
-
-            public Configurer Permit(TTrigger trigger, TState changeTo, Action execute)
-                => Permit(trigger, changeTo, () => { execute(); return true; });
-
-            public Configurer Permit(TTrigger trigger, TState changeTo)
-                => Permit(trigger, changeTo, () => true);
-
-            public Configurer Permit(TTrigger trigger, TState changeTo, Func<bool> condition)
-            {
-                Machine.Permits.Add(new Permit(State, trigger, changeTo, condition));
-
-                return this;
+                Machine.Permits.Add(new Permit(State, trigger, changeTo, execute));
             }
         }
     }
